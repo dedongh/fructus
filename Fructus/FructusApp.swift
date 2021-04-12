5241272018b9eb55a075c383e005d6764872a710
braemma <asiamahemma95@gmail.com>
2021-04-12 09:06:09 +0000

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Bra Emma on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
